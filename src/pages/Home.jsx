8dd2353764b2b6e2872e501eef1a0fe80be49a6b
sejanmiah <sejankhan931@gmail.com>
2025-09-01 01:02:15 +0400

    needed web assests Added in the file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1 className='text-9xl'>Home psge</h1>
+            <h1 className='text-9xl'>Home Page</h1>
         </div>
     );
 };
