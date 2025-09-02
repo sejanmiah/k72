6cbc74c7704eafb4a149c87a97c80c52dd57ff89
sejanmiah <sejankhan931@gmail.com>
2025-09-02 23:06:32 +0400

    Header and Agence page design improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
 import Agence from "./pages/Agence";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navigation/Navbar";
+import FullScreenNav from "./components/Navigation/FullScreenNav";
+import NavContext from "./context/NavContext";
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home></Home>}>
-          {" "}
-        </Route>
-        <Route path="/project" element={<Projects></Projects>}></Route>
-        <Route path="/agence" element={<Agence></Agence>}></Route>
-      </Routes>
+    <div className="overflow-x-hidden">
+      <NavContext>
+        <Navbar />
+        <FullScreenNav />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/agence" element={<Agence />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </NavContext>
     </div>
   );
 };
